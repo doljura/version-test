32cbad6ece24f5df1a0342e9344a52c47017c962
ydolinskyi <ydolinskyi@prismhr.com>
2020-02-10 17:37:49 +0200

    fix: DSC-223 test commit fix 1000

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const test = "test";
 const test2 = "test2";
-const test3 = "test3";
-const test4 = "test4";
-const test5 = "test5";
-const test6 = "test6";
-const test7 = "test7";
-const test8 = "test8";
+
 
